9f2861d0df6300fbd0a37cfbd97f7a1e308e37d2
theliang <1415123344@qq.com>
2019-04-15 17:47:55 +0800

    Eureka服务发现

--- micro-service-cloud/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ micro-service-cloud/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.Dept;
 import com.atguigu.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService service;
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -25,6 +29,20 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list() {
         return service.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
     }
 
 }
